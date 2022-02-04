d3100151f938030b3767c1afba1dbb0ff249316f
tanmayrai21 <vabhavrai98@gmail.com>
2022-02-04 18:27:05 +0530

    intro component

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
 
-import './Intro.css';
+import { meal } from '../../constants';
+import './Intro.scss';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const vidRef = React.useRef();
+
+  return (
+    <div className="intro">
+      <video ref={vidRef} src={meal} type="video/mp4" loop controls={false} autoPlay={true} muted />
+      <div className="intro__overlay flex__center"></div>
+    </div>
+  );
+};
 
 export default Intro;
